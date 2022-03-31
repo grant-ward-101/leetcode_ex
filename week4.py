a54b42777688049fc2df29963cf246d5de3678dd
Sinh <adamlavine110@gmail.com>
2022-04-01 00:42:39 +0700

    new file

---
+++ week4.py
@@ -0,0 +1,25 @@
+import bisect
+import copy
+import functools
+import itertools
+import math
+import operator
+import random
+from typing import List
+import numpy as np
+import collections
+import string
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
